b586cb7bb77a03cb5c924aa94d9dfb4f7fd53948
Abhishek Sahu <asahu532@gmail.com>
2023-10-10 08:55:57 +0530

    Frontend started, Header section completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
-import Header from '../components/Header.js'
+import Header from './components/Header'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+
+
 function App() {
   return (
-    <Header />
+    <Router> <Header /></Router>
+
   );
 }
 
