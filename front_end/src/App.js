f9227c5c65c58671ec532e052efb5ac1ff5cbd82
Abhishek Sahu <asahu532@gmail.com>
2023-10-17 23:53:30 +0530

    Solved the react dependency tree

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Header from './components/Header'
 import WebFont from 'webfontloader'
 import React from 'react';
@@ -7,30 +6,44 @@ import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home.js'
 // import Loader from './components/Loader/Loader'
 import ProductDetails from './components/Product/ProductDetails.js'
-
+// import { Route, Routes } from 'react-router-dom';
 
 function App() {
-//font will load before the page load
-  React.useEffect(()=>{
-WebFont.load({
-  google:{
-    families:['Roboto','Droid Sans','Chilanka']
-  },
-},[])
+  //font will load before the page load
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+      },
+    }, [])
   })
   return (
-    <Router>
-      
-<Header />
-<Routes>
-<Route exact path='/' Component={ Home } />
-<Route exact path='/product/:id' Component={ ProductDetails } />
 
-</Routes>
 
-<Footer/>
+    <div className='App'>
+      <Header />
 
-    </Router>
+      <Home/>
+      <ProductDetails />
+      {/* <Routes>
+        <Route exact path='/' Component={Home} />
+        <Route exact path='/product/:id' Component={ProductDetails} />
+
+      </Routes> */}
+      <Footer />
+    </div>
+
+    //     <Router>
+
+    // 
+    // {/* <Home /> */}
+    // {/* <Switch>  */}
+    //   {/* MADE SOME CHANGES DUE TO DEGREADE THE REACT AND REACT-DOM */}
+
+    // {/* </Switch> */}
+    // 
+
+    //     </Router>
   );
 }
 
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './Store.js'
 
+import { BrowserRouter } from "react-router-dom";
 import {positions, transitions, Provider as AlertProvider} from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 const options ={
   timeout: 5000,
   positions: positions.BOTTOM_CENTER,
   transitions: transitions.SCALE
 }
 
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options} >
 
-    <App />
+    <AlertProvider template={AlertTemplate} {...options} >
+<BrowserRouter>
+<App />
+</BrowserRouter>
+    
+
     </AlertProvider>
  
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
