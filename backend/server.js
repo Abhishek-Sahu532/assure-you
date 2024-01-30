d7cd0d0206e2e2f68e8addad0161415d3f4c7404
Abhishek-Sahu532 <asahu532@gmail.com>
2024-01-30 16:51:59 +0530

    Trying to deploy backend in vercel , due to slow rendering through Onrender

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,34 @@
-const app = require('./app');
+const app = require("./app");
 
-const connectDatabase = require('./config/db')
-const cloudinary = require('cloudinary')
+const connectDatabase = require("./config/db");
+const cloudinary = require("cloudinary");
 
 //HANDLING UNCAUGHT EXCEPTION
-process.on('uncaughtException',(err)=>{
-    console.log(`error:${err.message}`);
-    process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`error:${err.message}`);
+  process.exit(1);
+});
 //config
-if(process.env.NODE_ENV !== 'PRODUCTION'){
-    require('dotenv').config({path: 'backend/config/.env'});
-}
 
+require("dotenv").config({ path: "backend/config/.env" });
 
 // DATABASE CONNECTION
-connectDatabase()
+connectDatabase();
 
 cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
-const server = app.listen(process.env.PORT, ()=>{
-    console.log('Server is connected on port ', process.env.PORT )
-})
-
-
-
-
-
+const server = app.listen(process.env.PORT, () => {
+  console.log("Server is connected on port ", process.env.PORT);
+});
 
 //Unhandled promise rejection
-process.on('unhandledRejection', (err)=>{
-    console.log(`Error :  ${err.message}`);
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+process.on("unhandledRejection", (err) => {
+  console.log(`Error :  ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
